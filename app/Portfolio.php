1c7716c163ae48da27fc0b78e12b9b5e93e4b4e6
vincent <vincentnyamongo95@gmail.com>
2020-09-22 14:06:51 +0300

    guarded

--- app/Portfolio.php
+++ app/Portfolio.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Portfolio extends Model
 {
     //
+    protected $guarded= [];
 }
--- app/Testimonal.php
+++ app/Testimonal.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Testimonal extends Model
 {
     //
+    protected $guarded= [];
 }
