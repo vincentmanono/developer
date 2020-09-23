c3bdf2d6938e0bd800a81661d06d72ee0c601423
Abrahamkivosh <abrahamkivosh@gmail.com>
2020-09-23 19:44:24 +0300

    modified portfoliouserseeder file

--- database/factories/PortfolioUserFactory.php
+++ database/factories/PortfolioUserFactory.php
@@ -2,13 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\User;
 use App\Portfolio;
 use App\PortfolioUser;
 use Faker\Generator as Faker;
 
 $factory->define(PortfolioUser::class, function (Faker $faker) {
     return [
-        'portfolio_id'=>function(){return App\Portfolio::all()->random();},
-        'user_id'=>function(){return App\User::all()->random();}
+        'portfolio_id'=>function(){return Portfolio::all()->random();},
+        'user_id'=>function(){return User::all()->random();}
     ];
 });
--- database/seeds/PortfolioUserSeeder.php
+++ database/seeds/PortfolioUserSeeder.php
@@ -12,6 +12,6 @@ class PortfolioUserSeeder extends Seeder
     public function run()
     {
         //
-        factory(App\PortfolioUser::class,25);
+        factory(App\PortfolioUser::class,25)->create();
     }
 }
