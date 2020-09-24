5e847dbc2064a9bc6e0df35e125e47042d8b1e56
Abrahamkivosh <abrahamkivosh@gmail.com>
2020-09-25 00:40:10 +0300

    able to show css in both parent page and child page in client and admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,48 +7,48 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
   <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
-  <link rel="stylesheet" href="dist/css/custome.css">
+  <link rel="stylesheet" href="{{ asset('plugins/jqvmap/jqvmap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('dist/css/custome.css') }}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
   <!-- summernote -->
-  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.css') }}">
 
   {{-- table --}}
-  <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
 
 
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- daterange picker -->
-    <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
     <!-- iCheck for checkboxes and radio inputs -->
-    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <!-- Bootstrap Color Picker -->
-    <link rel="stylesheet" href="plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css">
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css') }}">
     <!-- Tempusdominus Bbootstrap 4 -->
-    <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
     <!-- Select2 -->
-    <link rel="stylesheet" href="plugins/select2/css/select2.min.css">
-    <link rel="stylesheet" href="plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
     <!-- Bootstrap4 Duallistbox -->
-    <link rel="stylesheet" href="plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css') }}">
 
     <!-- summernote -->
-    <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
+    <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.css') }}">
 
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
@@ -86,45 +86,45 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- ChartJS -->
-<script src="plugins/chart.js/Chart.min.js"></script>
+<script src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
 <!-- Sparkline -->
-<script src="plugins/sparklines/sparkline.js"></script>
+<script src="{{ asset('plugins/sparklines/sparkline.js') }}"></script>
 <!-- JQVMap -->
-<script src="plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="{{ asset('plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+<script src="{{ asset('plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
 <!-- jQuery Knob Chart -->
-<script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="{{ asset('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
 <!-- daterangepicker -->
-<script src="plugins/moment/moment.min.js"></script>
-<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
+<script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 <!-- Summernote -->
-<script src="plugins/summernote/summernote-bs4.min.js"></script>
+<script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
 <!-- overlayScrollbars -->
-<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.js"></script>
+<script src="{{ asset('dist/js/adminlte.js') }}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
+<script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<script src="{{ asset('dist/js/demo.js') }}"></script>
 
 <!-- DataTables -->
-<script src="plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
 {{-- table --}}
 <script>
     $(function () {
--- resources/views/layouts/mainapp.blade.php
+++ resources/views/layouts/mainapp.blade.php
@@ -1,148 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<!-- Mirrored from tf.yellowcrow.online/walton/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 15 Sep 2020 15:12:53 GMT -->
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,800" rel="stylesheet"> -->
     <title>Portfolio Project - Walton Consulting Template</title>
-    <link rel="stylesheet" href="assets/css/vendor.css">
-    <link rel="stylesheet" href="assets/css/style-1.css" id="template-skin">
+    <link rel="stylesheet" href="{{ asset('assets/css/vendor.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style-1.css') }}" id="template-skin">
 </head>
 
 <body>
     <!-- Preload start -->
-    {{-- <div id="loader-wrapper">
-        <div id="loader"></div>
-        <div class="loader-section section-left"></div>
-        <div class="loader-section section-right"></div>
-    </div> --}}
     <!-- Preload end -->
     <!-- Settings start -->
-    {{-- <div id="template-settings" class="template-settings">
-        <i id="template-settings-open" class="fa fa-cog"></i>
-        <div class="template-settings-pane skin-pane">
-            <h4>choose skin</h4>
-            <ul>
-                <li>
-                    <a href="#" data-skin="1" class="skin-1">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-skin="2" class="skin-2">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-skin="3" class="skin-3">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-skin="4" class="skin-4">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-skin="5" class="skin-5">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-skin="6" class="skin-6">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-skin="7" class="skin-7">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-skin="8" class="skin-8">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-skin="9" class="skin-9">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-skin="10" class="skin-10">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-skin="11" class="skin-11">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" data-skin="12" class="skin-12">
-                        <span class="c1"></span>
-                        <span class="c2"></span>
-                        <span class="c3"></span>
-                        <span class="c4"></span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div class="template-settings-pane layout-pane">
-            <h4>page layout</h4>
-            <ul>
-                <li>
-                    <label>
-              <input type="radio" name="choose-page-layout" value="wide" checked="checked">
-              <span>Wide</span>
-            </label>
-                </li>
-                <li>
-                    <label>
-              <input type="radio" name="choose-page-layout" value="boxed">
-              <span>Boxed</span>
-            </label>
-                </li>
-            </ul>
-        </div>
-    </div> --}}
+
     <!-- Settings end -->
     <!-- To the top start -->
     <div id="to-the-top" class="to-the-top">
@@ -174,12 +46,9 @@
             <!-- Footer end -->
         </div>
     </div>
-    <script src="assets/js/vendor.js"></script>
-    <script src="assets/js/framework.js"></script>
+    <script src="{{ asset('assets/js/vendor.js') }}"></script>
+    <script src="{{ asset('assets/js/framework.js') }}"></script>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpVQEdCmd9aY_-N6920-wilfScyBwS-Lw&amp;callback=initMap"></script>
-    <script src="assets/js/template.js"></script>
+    <script src="{{ asset('assets/js/template.js') }}"></script>
 </body>
-
-<!-- Mirrored from tf.yellowcrow.online/walton/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 15 Sep 2020 15:16:50 GMT -->
-
 </html>
