04352245980ccde2185a9fed64a597ed705e3f31
vincentmanono <vincentnyamongo95@gmail.com>
2021-02-02 13:09:05 +0300

    about us completed

--- resources/views/client/about_us.blade.php
+++ resources/views/client/about_us.blade.php
@@ -23,19 +23,18 @@
             <div class="col-md-6">
                 <div class="full paddding_left_15">
                     <div class="heading_main text_align_left">
-                       <h2>About</h2>
+                       <h2>Overview</h2>
                     </div>
                 </div>
                 <div class="full paddding_left_15">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                </div>
-                <div class="full paddding_left_15">
-                    <a class="main_bt" href="#">Read More ></a>
+                    <p>
+                      Lagaster is a company that was founded on the basis if website and android application development. Lagaster has been a company where clients interact with the developers and provide essential details required for their system to be designed and developed.
+                    </p>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="full text_align_right_img">
-                    <img src="images/img3.png" alt="#" />
+                    <img height="110%" width="100%" src="{{ asset('assets/images/abt.jpg') }}" alt="#" />
                 </div>
             </div>
         </div>
@@ -49,47 +48,25 @@
         <div class="row">
             <div class="col-md-6">
                 <div class="full text_align_right_img">
-                    <img src="images/img1.png" alt="#" />
+                    <img height="110%" width="100%" src="{{ asset('assets/images/slider.jpg') }}" alt="#" />
                 </div>
             </div>
             <div class="col-md-6 layout_padding">
                 <div class="full paddding_left_15">
                     <div class="heading_main text_align_left">
-                       <h2><span class="theme_color">Welcome</span> To Exchange</h2>
+                       <h2><span class="theme_color">What</span>We Do</h2>
                     </div>
                 </div>
                 <div class="full paddding_left_15">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                </div>
-                <div class="full paddding_left_15">
-                    <a class="main_bt" href="#">About more ></a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- end section -->
-
-<!-- section -->
-<div class="section white_fonts" style="background: #111;">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6" style="background: #fff;">
-                <div class="full text_align_right_img">
-                    <img src="images/img2.png" alt="#" />
-                </div>
-            </div>
-            <div class="col-md-6 layout_padding">
-                <div class="full paddding_left_15">
-                    <div class="heading_main text_align_left">
-                       <h2><span class="theme_color">Apply for</span> Exchange</h2>
-                    </div>
-                </div>
-                <div class="full paddding_left_15">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                </div>
-                <div class="full paddding_left_15">
-                    <a class="main_bt" href="#">Exchange ></a>
+                    <p>
+                        We design and develop mobile applications that offer the best digital experience.
+                    </p>
+                    <p>
+                        We also develop professional and competent website design using current technology.
+                    </p>
+                    <p>
+                        We offer the best design and installation of well functioning networks and perform troubleshooting to networks.
+                    </p>
                 </div>
             </div>
         </div>
